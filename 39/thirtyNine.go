b1a7110d61e22447ac3ee1ef87a5b2d9f2870056
lianjie1949@gmail.com <lianjie1949@gmail.com>
2019-04-18 20:50:50 +0800

    complete exercise 39

---
+++ 39/thirtyNine.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+	//	"os"
+	//	"bufio"
+	//	"strings"
+	//	"strconv"
+)
+
+type Name struct {
+	FirstName string
+	LastName  string
+}
+
+func (name Name) fullName() (_fullName string) {
+	return name.FirstName + " " + name.LastName
+}
+
+type User struct {
+	Name
+	Position      string
+	SepartionData string
+}
+
+var users []User
+
+func init() {
+	name1 := Name{"John", "Johnson"}
+	name2 := Name{"Tou", "Xiong"}
+	name3 := Name{"Michaela", "Michaelson"}
+	name4 := Name{"Jake", "Jacobon"}
+	name5 := Name{"Jacquelyn", "Jackson"}
+	name6 := Name{"Sally", "Weber"}
+
+	user1 := User{name1, "Manager", "2016-12-31"}
+	user2 := User{name2, "Software Engineer", "2016-10-05"}
+	user3 := User{name3, "District Manager", "2015-12-19"}
+	user4 := User{name4, "Programmer", ""}
+	user5 := User{name5, "DBA", ""}
+	user6 := User{name6, "Web Developer", "2015-12-08"}
+
+	users = []User{user1, user2, user3, user4, user5, user6}
+}
+
+type byName []User
+
+func (us byName) Len() int { return len(us) }
+func (us byName) Less(i, j int) bool {
+	return us[i].fullName() < us[j].fullName()
+}
+func (us byName) Swap(i, j int) {
+	us[i], us[j] = us[j], us[i]
+}
+
+func (us byName) Sort() { sort.Sort(us) }
+
+func main() {
+	byName(users).Sort()
+	for _, user := range users {
+		fmt.Println(user.fullName())
+	}
+}
